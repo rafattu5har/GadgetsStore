16e5a98d5dab86269c09acaf107747977b737b98
Rafat Rahman Tushar <rafat.tushar@northsouth.edu>
2019-11-26 22:47:58 +0600

    Cart Page

---
+++ Project/Codes/js/cart.js
@@ -0,0 +1,15 @@
+function selectAll() {
+        var items = document.getElementsByName('acs');
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].type == 'checkbox')
+                items[i].checked = true;
+        }
+    }
+
+    function UnSelectAll() {
+        var items = document.getElementsByName('acs');
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].type == 'checkbox')
+                items[i].checked = false;
+        }
+    }
