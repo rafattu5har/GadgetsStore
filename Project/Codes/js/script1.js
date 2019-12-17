fa6a039e4933fffea48547f566b9767fd4191ad2
ShobaBanik <33699958+ShobaBanik@users.noreply.github.com>
2019-12-17 11:44:09 +0600

    Create script1.js

---
+++ Project/Codes/js/script1.js
@@ -0,0 +1,21 @@
+function myFunction()
+{
+	document.getElementById('demo').style.display='block';
+}
+
+
+function myFunction1() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+function pageRedirect() {
+
+	window.location.href = "https://www.tutorialrepublic.com/";
+
+}
