9d2627052d520337480bd50474eebcdc718ed863
ShobaBanik <33699958+ShobaBanik@users.noreply.github.com>
2019-12-19 20:46:12 +0600

    My prodect page update

--- Project/Codes/product.php
+++ Project/Codes/product.php
@@ -2,24 +2,29 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <script src="script.js" type="text/javascript"> </script>
+    <script src="js/script1.js" type="text/javascript"> </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style1.css">
     <title> Gadget Store </title>
   </head>
   <body>
     <h1>Gadget store</h1>
-    <div class="active">
-      <a href="#">Home</a>
+    <div class="topnav" id="myTopnav">
+      <a href="#" class="active">Home</a>
       <a href="#">Product</a>
       <a href="#">About</a>
       <a href="#">Carts</a>
+      <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction1()">&#9776;</a>
+
     </div>
-    
+
+    </div>
+
     <div>
       <p><i>Samsung A50</i></p>
       <p><i>Price: 23000/-</i></p>
-      
+
       <p id="demo" style="display:none">Memory, Storage and SIM: 4GB RAM | 64GB internal memory expandable up to      512GB | Dual SIM (nano+nano) dual-standby (4G+4G)
         Android Pie V9.0 operating system with 2.29 Ghz Exynos 9610 Octa Core Processor
         4000mAH lithium-ion battery with Type-C fast charging | 15W Charger in the box
@@ -27,13 +32,13 @@
         25MP + 5MP + 8MP with F1.7, F2.2 wide Angle and F2.2 with Flash | 25 MP front camers with F2.0</p>
       <button type="button1" onclick="myFunction()">Product Details</button>
       <div class="container">
-        <img src="images/abd.jpg" height="120px" width="180px" halt="Samsung Mobile">
+        <img src="images/abd.jpg" alt="Samsung Mobile">
       <div class="middle">
         <div class="text">Samsung A50</div>
       </div>
 
       </div>
-      <button class="button1">Add to cart</button>
+      <button class="button1" onclick="pageRedirect()">Add to cart</button>
     </div>
 
 
